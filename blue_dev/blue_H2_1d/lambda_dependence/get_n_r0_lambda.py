2aae3031f06db62b83f5e54a78e68edfd2a76a61
Ryan Pederson <vgnc@vt.edu>
2020-04-10 11:20:54 -0700

    blue_potentials

---
+++ blue_dev/blue_H2_1d/lambda_dependence/get_n_r0_lambda.py
@@ -0,0 +1,56 @@
+import single_electron, ext_potentials
+import blue_potentials
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
+from scipy import stats
+import functools
+import sys
+
+# plotting parameters
+params = {'mathtext.default': 'default'}
+plt.rcParams.update(params)
+plt.rcParams['axes.axisbelow'] = True
+fig_size = plt.rcParams["figure.figsize"]
+fig_size[0] = 9
+fig_size[1] = 6
+plt.rcParams["figure.figsize"] = fig_size
+fig, ax = plt.subplots()
+
+
+def get_blue_potential(grids, pot, r0):
+    return pot - ext_potentials.exp_hydrogenic(grids - r0)
+
+
+
+if __name__ == '__main__':
+    h = 0.08
+    grids = np.arange(-256, 257) * h
+    potentials = np.load("H2_data/potentials.npy")
+    potentials = potentials[:40]
+
+    n_r0_R = []
+    for i, potential in enumerate(potentials):
+        print(i)
+        n_r0 = []
+        for r0 in grids:
+            solver = single_electron.EigenSolver(grids,
+                                                 potential_fn=functools.partial(
+                                                     get_blue_potential,
+                                                     pot=potential, r0=r0),
+                                                 boundary_condition='open',
+                                                 num_electrons=1)
+            solver.solve_ground_state()
+
+            E0 = solver.eigenvalues[0]
+            # print("E0 = ", E0)
+
+            n = solver.density
+
+            n_r0.append(n)
+
+        n_r0 = np.asarray(n_r0)
+        n_r0_R.append(n_r0)
+
+    n_r0_R = np.asarray(n_r0_R)
+    np.save('n_r0_R', n_r0_R)
---
+++ blue_dev/blue_potentials.py
@@ -0,0 +1,25 @@
+import ext_potentials
+import numpy as np
+from scipy import special
+from scipy import integrate
+from scipy import optimize
+import math
+
+def blue_helium(grids, r0, Z=2, lam =1):
+    vp = []
+    for r in grids:
+        if np.abs(r) < r0:
+            vp.append((-Z / np.abs(r)) + (lam / r0))
+        else:
+            vp.append((-Z + lam) / np.abs(r))
+
+    return np.asarray(vp)
+
+
+def blue_helium_1d(grids, r0, Z=2, lam=1):
+    return ext_potentials.exp_hydrogenic(grids, Z=Z) - lam * ext_potentials.exp_hydrogenic(grids - r0)
+
+
+def helium(grids, Z=2):
+    return -Z / (np.abs(grids))
+

eed5c7735fcd259434f77ea07692f9d70467006d
Ryan Pederson <vgnc@vt.edu>
2020-04-12 19:50:52 -0700

    plotted quantities and Ec, Tc values

--- blue_dev/blue_H2_1d/lambda_dependence/get_U_xc_lambda.py
+++ blue_dev/blue_H2_1d/lambda_dependence/get_U_xc_lambda.py
@@ -13,8 +13,6 @@ fig_size[1] = 6
 plt.rcParams["figure.figsize"] = fig_size
 fig, ax = plt.subplots()
 
-
-
 if __name__ == '__main__':
     h = 0.08
     grids = np.arange(-256, 257) * h
@@ -24,31 +22,83 @@ if __name__ == '__main__':
 
     # get stretched H2 values
     pot = potentials[50]
-    n = dmrg_density[50]
-    n = n_HF
+    n_dmrg = dmrg_density[50]
 
     lambda_list = np.linspace(0, 1, 11)
 
-    v_h = functionals.hartree_potential(grids,n)
-    U = 0.5*np.sum(v_h*n)*h
-    print('U = ', U)
+    v_h = functionals.hartree_potential(grids, n_HF)
+    U_HF = 0.5 * np.sum(v_h * n_HF) * h
+    print('Ex_HF = ', -U_HF/2)
 
-    U_xc_lam = []
+    v_h_dmrg = functionals.hartree_potential(grids, n_dmrg)
+    U_dmrg = 0.5 * np.sum(v_h_dmrg * n_dmrg) * h
+    print('Ex_dmrg = ', -U_dmrg/2)
+
+    U_xc_lam_HF = []
+    U_xc_lam_dmrg = []
     for i, lam in enumerate(lambda_list):
         print("lam = ", lam)
-        n2_r0 = np.load("n_r0_lambda_HF.npy")[i]
 
-        v_h_n_ee = functionals.get_v_n_xc(grids,n2_r0)
-        V_ee_blue = 0.5*np.sum(v_h_n_ee*n)*h
+        if lam == 0.0 and False:
+            n2_r0_dmrg = np.load("n_r0_lambda_HF.npy")[i]
 
-        print("U_xc_blue(lam) = ", V_ee_blue - U)
-        U_xc_lam.append(V_ee_blue - U)
+            plt.plot(grids, 2*n2_r0_dmrg[0], label=r'density from $ v_{ext} + (1/2)v_H[n^{DMRG}]$')
+            plt.plot(grids, n_dmrg, label=r'$n^{DMRG}$')
+            plt.legend()
+            plt.show()
 
-    U_xc_lam = np.asarray(U_xc_lam)
 
-    plt.plot(lambda_list, U_xc_lam)
+        # using HF density
+        n2_r0_HF = np.load("n_r0_lambda_HF.npy")[i]
+        v_h_n_HF_ee = functionals.get_v_n_xc(grids, n2_r0_HF)
+        V_ee_blue_HF = 0.5 * np.sum(v_h_n_HF_ee * n_HF) * h
+
+        # print("U_xc_blue(lam) = ", V_ee_blue_HF - U_HF)
+        U_xc_lam_HF.append(V_ee_blue_HF - U_HF)
+
+        # using dmrg density
+        n2_r0_dmrg = np.load("n_r0_lambda.npy")[i]
+        v_h_n_dmrg_ee = functionals.get_v_n_xc(grids, n2_r0_dmrg)
+        V_ee_blue_dmrg = 0.5 * np.sum(v_h_n_dmrg_ee * n_dmrg) * h
+
+        # print("U_xc_blue(lam) = ", V_ee_blue_HF - U_HF)
+        U_xc_lam_dmrg.append(V_ee_blue_dmrg - U_dmrg)
+
+    U_xc_lam_HF = np.asarray(U_xc_lam_HF)
+    U_xc_lam_dmrg = np.asarray(U_xc_lam_dmrg)
+
+    # HF values
+    E_xc_HF = np.sum(U_xc_lam_HF) / (len(lambda_list))
+    print('E_xc = ', E_xc_HF)
+
+    E_c_HF = E_xc_HF - (-U_HF / 2)
+    U_c_HF = U_xc_lam_HF[-1] - (-U_HF / 2)
+    T_c_HF = E_c_HF - U_c_HF
+    print('U_c_HF = ', U_c_HF)
+    print('E_c_HF = ', E_c_HF)
+    print('T_c_HF = ', T_c_HF)
+    print()
+
+    # DMRG values
+    E_xc_dmrg = np.sum(U_xc_lam_dmrg) / (len(lambda_list))
+    print('E_xc = ', E_xc_dmrg)
+
+    E_c_dmrg = E_xc_dmrg - (-U_dmrg / 2)
+    U_c_dmrg = U_xc_lam_dmrg[-1] - (-U_dmrg / 2)
+    T_c_dmrg = E_c_dmrg - U_c_dmrg
+    print('U_c_dmrg = ', U_c_dmrg)
+    print('E_c_dmrg = ', E_c_dmrg)
+    print('T_c_dmrg = ', T_c_dmrg)
+
+    plt.plot(lambda_list, U_xc_lam_HF, label='using $n^{HF}$')
+    plt.plot(lambda_list, U_xc_lam_dmrg, label='using $n^{DMRG}$')
+
+    # plot our limits
+    plt.scatter([0,1], [-U_dmrg/2, U_xc_lam_dmrg[-1]], color = 'red', label = '$E_x[n^{DMRG}]$ and $U^B_{xc}(\lambda = 1)[n^{DMRG}]$')
+
 
     plt.ylabel('$U^B_{xc}(\lambda)$', fontsize=18)
     plt.xlabel('$\lambda$', fontsize=18)
     plt.grid(alpha=.4)
+    plt.legend(fontsize=16)
     plt.show()

37c3fb489ad1bbea8aef6163ce941903ca4a52a4
Ryan Pederson <vgnc@vt.edu>
2020-04-12 21:25:10 -0700

    fixes to U_xc_lam = 1

--- blue_dev/blue_H2_1d/lambda_dependence/get_U_xc_lambda.py
+++ blue_dev/blue_H2_1d/lambda_dependence/get_U_xc_lambda.py
@@ -28,11 +28,11 @@ if __name__ == '__main__':
 
     v_h = functionals.hartree_potential(grids, n_HF)
     U_HF = 0.5 * np.sum(v_h * n_HF) * h
-    print('Ex_HF = ', -U_HF/2)
+    print('Ex_HF = ', -U_HF / 2)
 
     v_h_dmrg = functionals.hartree_potential(grids, n_dmrg)
     U_dmrg = 0.5 * np.sum(v_h_dmrg * n_dmrg) * h
-    print('Ex_dmrg = ', -U_dmrg/2)
+    print('Ex_dmrg = ', -U_dmrg / 2)
 
     U_xc_lam_HF = []
     U_xc_lam_dmrg = []
@@ -42,11 +42,11 @@ if __name__ == '__main__':
         if lam == 0.0 and False:
             n2_r0_dmrg = np.load("n_r0_lambda_HF.npy")[i]
 
-            plt.plot(grids, 2*n2_r0_dmrg[0], label=r'density from $ v_{ext} + (1/2)v_H[n^{DMRG}]$')
+            plt.plot(grids, 2 * n2_r0_dmrg[0],
+                     label=r'density from $ v_{ext} + (1/2)v_H[n^{DMRG}]$')
             plt.plot(grids, n_dmrg, label=r'$n^{DMRG}$')
             plt.legend()
             plt.show()
-
 
         # using HF density
         n2_r0_HF = np.load("n_r0_lambda_HF.npy")[i]
@@ -83,19 +83,20 @@ if __name__ == '__main__':
     E_xc_dmrg = np.sum(U_xc_lam_dmrg) / (len(lambda_list))
     print('E_xc = ', E_xc_dmrg)
 
-    E_c_dmrg = E_xc_dmrg - (-U_dmrg / 2)
-    U_c_dmrg = U_xc_lam_dmrg[-1] - (-U_dmrg / 2)
+    E_c_dmrg = E_xc_dmrg - U_xc_lam_dmrg[0]
+    U_c_dmrg = U_xc_lam_dmrg[-1] - U_xc_lam_dmrg[0]
     T_c_dmrg = E_c_dmrg - U_c_dmrg
     print('U_c_dmrg = ', U_c_dmrg)
     print('E_c_dmrg = ', E_c_dmrg)
     print('T_c_dmrg = ', T_c_dmrg)
+    print('E_x_dmrg_blue = ', U_xc_lam_dmrg[0])
 
     plt.plot(lambda_list, U_xc_lam_HF, label='using $n^{HF}$')
     plt.plot(lambda_list, U_xc_lam_dmrg, label='using $n^{DMRG}$')
 
     # plot our limits
-    plt.scatter([0,1], [-U_dmrg/2, U_xc_lam_dmrg[-1]], color = 'red', label = '$E_x[n^{DMRG}]$ and $U^B_{xc}(\lambda = 1)[n^{DMRG}]$')
-
+    plt.scatter([0, 1], [-U_dmrg / 2, U_xc_lam_dmrg[-1]], color='red',
+                label='$E_x[n^{DMRG}]$ and $U^B_{xc}(\lambda = 1)[n^{DMRG}]$')
 
     plt.ylabel('$U^B_{xc}(\lambda)$', fontsize=18)
     plt.xlabel('$\lambda$', fontsize=18)

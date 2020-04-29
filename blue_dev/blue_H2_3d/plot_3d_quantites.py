7fe81705b1713d8959cb7f4b4235fcb0b6afef70
Ryan Pederson <vgnc@vt.edu>
2020-04-28 17:21:45 -0700

    todo: read cyrus data

--- blue_dev/blue_H2_3d/plot_3d_quantites.py
+++ blue_dev/blue_H2_3d/plot_3d_quantites.py
@@ -32,31 +32,51 @@ def get_Vpp(R):
     return 1. / np.abs(R)
 
 
-if __name__ == '__main__':
-    R, T = txt_file_to_array('H2_from_srwhite/T.dat')
-    R, T_s = txt_file_to_array('H2_from_srwhite/T_s.dat')
-    R, V_ext_plus_T = txt_file_to_array('H2_from_srwhite/H1en.dat')
-    R, U = txt_file_to_array('H2_from_srwhite/U.dat')
-    R, E = txt_file_to_array('H2_from_srwhite/E.dat')
-    R, Vee = txt_file_to_array('H2_from_srwhite/Vee.dat')
-
-    V_ext = V_ext_plus_T - 2 * T
-
-    plt.plot(R, E + get_Vpp(R), label='exact')
-    # plt.plot(R, T + V_ext + Vee + get_Vpp(R)) # check.. should be = E
-    plt.plot(R, 2 * T_s + V_ext + U + get_Vpp(R), label='1')
-
-    # V_ext = V_ext_plus_T - T
-    # plt.plot(R, T_s + V_ext + U + get_Vpp(R), label='2')
-
-    # Exc
+def check_Exc_paper():
+    # Exc from paper
     R_idx_paper = [0, 2, 5, 10, 15]
     paper_Exc_vals = [-0.7857, -0.7024, -0.6174, -0.5543, -0.5642]
 
     for i, R_idx in enumerate(R_idx_paper):
         print(R[R_idx], '      ',
               paper_Exc_vals[i] - (E[R_idx] - (
-                      2 * T_s[R_idx] + V_ext[R_idx] + 2 * U[R_idx])))
+                      2 * T_s[R_idx] + V_ext[R_idx] + 2 * U_plus_Ex[R_idx])))
+
+
+def array_to_file(datafile_path, x_column, y_column, header):
+    datafile_path += '.dat'
+    data = np.array([x_column, y_column])
+    data = data.T
+    # here you transpose your data, so to have it in two columns
+
+    with open(datafile_path, 'w+') as datafile_id:
+        np.savetxt(datafile_id, data, fmt=['%d', '%d'], header=header,
+                   comments='')
+
+    return
+
+def array_to_plot(datafile_path, x, y, xlabel, ylabel):
+
+    return
+
+if __name__ == '__main__':
+    R, T = txt_file_to_array('H2_from_srwhite/T.dat')
+    R, T_s = txt_file_to_array('H2_from_srwhite/T_s.dat')
+    R, V_ext_plus_T = txt_file_to_array('H2_from_srwhite/H1en.dat')
+    R, U_plus_Ex = txt_file_to_array('H2_from_srwhite/U.dat')
+    R, E = txt_file_to_array('H2_from_srwhite/E.dat')
+    R, Vee = txt_file_to_array('H2_from_srwhite/Vee.dat')
+    V_ext = V_ext_plus_T - 2 * T
+
+    E_xc = E - (2 * T_s + V_ext + 2 * U_plus_Ex)
+    U_c = Vee - U_plus_Ex
+    T_c = 2 * T - 2 * T_s
+    E_c = E_xc - (-U_plus_Ex)
+    U_xc = (-U_plus_Ex) + U_c
+
+
+
+
 
     plt.xlabel('R')
     plt.legend()

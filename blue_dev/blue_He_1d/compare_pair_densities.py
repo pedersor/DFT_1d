d9522a6a7db531a2d8a5cadad7b11e92f4b04223
Ryan Pederson <vgnc@vt.edu>
2020-05-04 22:57:12 -0700

    Update compare_pair_densities.py
    proper plotting

--- blue_dev/blue_He_1d/compare_pair_densities.py
+++ blue_dev/blue_He_1d/compare_pair_densities.py
@@ -113,17 +113,6 @@ if __name__ == '__main__':
     avg_n_xc_blue = np.asarray(avg_n_xc_blue)
     avg_n_x_exact = np.asarray(avg_n_x_exact)
 
-    '''
-    # E_x check..
-    E_x = np.trapz(
-        avg_n_x_exact[256:] * -1 * ext_potentials.exp_hydrogenic(u_grids[256:]), u_grids[256:])
-    print('E_x = ', E_x)
-
-    v_H = functionals.hartree_potential(grids, n_dmrg)
-    U = 0.5*np.trapz(v_H*n_dmrg, grids)
-    print('E_x = -U/2 = ', -U/2.)
-    '''
-
     # symmetric plotting, |u|
     zero_u_idx = 256
     u_grids = u_grids[zero_u_idx:]
@@ -135,22 +124,35 @@ if __name__ == '__main__':
     avg_n_c_exact = avg_n_xc_exact - avg_n_x_exact
     avg_n_c_blue = avg_n_xc_blue - avg_n_x_exact
 
+
+    # plots --------
+    def do_plot():
+        plt.xlabel('$|u|$', fontsize=18)
+        plt.legend(fontsize=16)
+        plt.grid(alpha=0.4)
+        plt.show()
+
+
     # avg_n_xc
-    # plt.plot(u_grids, avg_n_xc_blue, label='blue')
-    # plt.plot(u_grids, avg_n_xc_exact, label='exact')
+    plt.plot(u_grids, avg_n_xc_blue,
+             label=r'$\left\langle n^{Blue}_{xc}(|u|) \right\rangle$')
+    plt.plot(u_grids, avg_n_xc_exact,
+             label=r'$\left\langle n^{Exact}_{xc}(|u|) \right\rangle$')
+    do_plot()
 
     # avg_n_xc weighted with Vee
     plt.plot(u_grids, avg_n_xc_blue * -1 * ext_potentials.exp_hydrogenic(
-        u_grids), label='blue')
+        u_grids), label=r'$\left\langle n^{Blue}_{xc}(|u|) \right\rangle v_{ee}(|u|)$')
     plt.plot(u_grids, avg_n_xc_exact * -1 * ext_potentials.exp_hydrogenic(
-        u_grids), label='exact')
-    plt.show()
+        u_grids), label=r'$\left\langle n^{Exact}_{xc}(|u|) \right\rangle v_{ee}(|u|)$')
+    do_plot()
 
     # avg_n_c weighted with Vee
     plt.plot(u_grids, avg_n_c_blue * -1 * ext_potentials.exp_hydrogenic(
-        u_grids), label='blue')
+        u_grids), label=r'$\left\langle n^{Blue}_{c}(|u|) \right\rangle v_{ee}(|u|)$')
     plt.plot(u_grids, avg_n_c_exact * -1 * ext_potentials.exp_hydrogenic(
-        u_grids), label='exact')
+        u_grids), label=r'$\left\langle n^{Exact}_{c}(|u|) \right\rangle v_{ee}(|u|)$')
+    do_plot()
 
     # U_c check:
     print('U_c_blue = ',
@@ -160,57 +162,19 @@ if __name__ == '__main__':
           np.trapz(avg_n_c_exact * -1 * ext_potentials.exp_hydrogenic(
               u_grids), u_grids))
 
-    plt.show()
+    # E_x check:
+    E_x = np.trapz(
+        avg_n_x_exact * -1 * ext_potentials.exp_hydrogenic(u_grids), u_grids)
+    print('E_x = ', E_x)
+
+    v_H = functionals.hartree_potential(grids, n_dmrg)
+    U = 0.5 * np.trapz(v_H * n_dmrg, grids)
+    print('E_x = -U/2 = ', -U / 2.)
 
     sys.exit()
 
-    x_value = 0.0
-    x_idx = np.where(grids == x_value)[0][0]
-
-    P_r_rp_idx = blue_pair_density.get_P_r_rp_idx(P_r_rp_raw, n=n_dmrg,
-                                                  x_idx=x_idx,
-                                                  h=h)
-
-    # blue ----------------------------
-    blue_CP = np.load('n_r0_0.npy')[0][x_idx]
-
-    plt.plot(grids, n_xc_blue_interp[x_idx](grids),
-             label='$n^{Blue}_{xc}(' + str(x_value) + ',x\prime)$')
-    plt.plot(grids, n_xc_exact_interp[x_idx](grids),
-             label='$n_{xc}(' + str(x_value) + ',x\prime)$')
-    plt.xlabel('$x\prime$', fontsize=16)
-    plt.legend(fontsize=16)
-    plt.grid(alpha=0.4)
-    plt.show()
-
-    plt.plot(grids, (blue_CP) - n_dmrg / 2,
-             label='$n^{Blue}_{c}(' + str(x_value) + ',x\prime)$')
-    plt.plot(grids, n_xc_exact[x_idx] + n_dmrg / 2,
-             label='$n_{c}(' + str(x_value) + ',x\prime)$')
-    plt.xlabel('$x\prime$', fontsize=16)
-    plt.legend(fontsize=16)
-    plt.grid(alpha=0.4)
-    plt.show()
-
-    # compare v_s of CP
-
-    v_s_CP_blue = two_el_exact.v_s_extention(grids, blue_CP, h)
-    v_s_CP_exact = two_el_exact.v_s_extention(grids,
-                                              (P_r_rp_idx / n_dmrg[x_idx]), h,
-                                              tol=1.1 * (10 ** (-4)))
-    plt.plot(grids, v_s_CP_blue,
-             label='$v^{CP, Blue}_s(' + str(x_value) + ',x\prime)$')
-    plt.plot(grids, v_s_CP_exact,
-             label='$v^{CP, Exact}_s(' + str(x_value) + ',x\prime)$')
-
-    plt.xlabel('$x\prime$', fontsize=16)
-    plt.legend(fontsize=16)
-    plt.grid(alpha=0.4)
-    plt.show()
-
-    sys.exit()
-    if __name__ == '__main__':
-        h = 0.08
+if __name__ == '__main__':
+    h = 0.08
     grids = np.arange(-256, 257) * h
 
     # (x = 0) or specific values

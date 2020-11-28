ab1023a06b0a4181c45303132722f8409dbd9ed4
Ryan J McCarty <43008464+RyanJMcCarty@users.noreply.github.com>
2020-11-28 14:00:36 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+#THIS IS FOR IF YOU DO A PYPI
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DFT_1d", # Replace with your own username
+    version="0.0.0",
+    author="R.P. etc...",
+    author_email="pedersor@uci.edu",
+    description="This is a python based 1D solver.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="URL_TO_REPO",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License", #TALK TO RYAN M IF YOU WANT LISCENCE ADVICE
+        "Operating System :: OS Independent", #IF fully python
+        "Development Status :: 4 - Beta", #IF actually beta at that point
+    ],
+    python_requires='>=3.6', #??
+)

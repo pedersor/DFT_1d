78ec8c1b69a01a37c7a9b13ddc02263aff9b93d4
Ryan Pederson <vgnc@vt.edu>
2021-01-12 18:11:45 -0800

    add hf

--- ksr/generate_dataset.py
+++ ksr/generate_dataset.py
@@ -1,11 +1,11 @@
 import numpy as np
-import ks_dft, functionals, ext_potentials
+import ks_dft, hf_scf, functionals, ext_potentials
 import functools
 import os
 import sys
 
 
-class LDA_atom_dataset():
+class Generate_dataset():
   """Obtain dataset for LDA-calculated systems. Current support is for atoms.
   """
 
@@ -78,6 +78,27 @@ class LDA_atom_dataset():
         solver.density, solver.zeta)
       self.xc_energy_densities.append(xc_energy_density / solver.density)
 
+  def run_hf_selected_ions(self):
+    exponential_hf = functionals.ExponentialHF(grids=grids)
+    for ((nuclear_charge, num_electron), center) in zip(self.selected_ions,
+                                                        self.locations):
+      v_ext = functools.partial(ext_potentials.exp_hydrogenic, Z=nuclear_charge,
+                                center=center)
+      solver = hf_scf.HF_Solver(self.grids, v_ext=v_ext,
+                                hf=exponential_hf,
+                                num_electrons=num_electron)
+      solver.solve_self_consistent_density(verbose=1)
+      print(
+        'finished: (Z, N_e) = (' + str(nuclear_charge) + ',' +
+        str(num_electron) + ')')
+
+      self.num_electrons.append(num_electron)
+      self.nuclear_charges.append([nuclear_charge])
+      self.total_energies.append(solver.E_tot)
+      self.densities.append(solver.density)
+      self.external_potentials.append(v_ext(self.grids))
+      self.xc_energies.append(solver.E_x)
+
   def save_dataset(self, out_dir):
     if not os.path.exists(out_dir):
       os.makedirs(out_dir)
@@ -95,23 +116,28 @@ class LDA_atom_dataset():
             self.external_potentials)
     np.save(os.path.join(out_dir, 'xc_energies.npy'),
             self.xc_energies)
-    np.save(os.path.join(out_dir, 'xc_energy_densities.npy'),
-            self.xc_energy_densities)
+
+    if self.xc_energy_densities:
+      np.save(os.path.join(out_dir, 'xc_energy_densities.npy'),
+              self.xc_energy_densities)
 
 
 if __name__ == '__main__':
+  """Get example dataset for KSR calculations."""
   h = 0.08
   grids = np.arange(-256, 257) * h
 
   # ions are identified by: atomic number Z, number of electrons
-  selected_ions = [(1, 1), (2, 1), (3, 1), (4, 1), (2, 2), (3, 2), (4, 2),
-                   (3, 3), (4, 3), (4, 4)]
+  selected_ions = [(1, 1), (2, 1), (3, 1), (4, 1)]
 
-  out_dir = os.path.join('ions', 'basic_all')
+  out_dir = os.path.join('ions', 'exact_one_el')
 
-  dataset = LDA_atom_dataset(grids, selected_ions)
-  dataset.run_lsda_selected_ions()
+  dataset = Generate_dataset(grids, selected_ions)
+  dataset.run_hf_selected_ions()
+
+  print('Total energies:')
   print(dataset.total_energies)
+  print('XC energies:')
   print(dataset.xc_energies)
 
   dataset.save_dataset(out_dir=out_dir)

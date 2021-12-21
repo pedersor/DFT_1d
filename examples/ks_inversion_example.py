17f02c56376e94da2ba8694e0239586840f0a21b
pedersor <pedersor@uci.edu>
2021-12-20 16:56:19 -0800

    updated ks example

--- examples/ks_inversion_example.py
+++ examples/ks_inversion_example.py
@@ -1,32 +1,52 @@
 import pathlib
 
+import jax
+from jax import tree_util
 import numpy as np
 import matplotlib.pyplot as plt
 
 from DFT_1d import ks_inversion
+from DFT_1d import functionals
 
 if __name__ == '__main__':
-  example_dir = pathlib.Path('../data/molecules/h_be_h')
-  idx = 50
+  example_dir = pathlib.Path('../data/molecules/li_h')
+  separation = 5.92  # in units Bohr
 
-  f_v_Z_fn = lambda _: np.load(example_dir / 'external_potentials.npy')[idx]
-  f_tar_density = np.load(example_dir / 'densities.npy')[idx]
-  num_electrons = np.load(example_dir / 'num_electrons.npy')[idx]
-  f_grids = np.load(example_dir / 'grids.npy')
+  example = {
+      'distances': np.load(example_dir / 'distances.npy'),
+      'external_potentials': np.load(example_dir / 'external_potentials.npy'),
+      'densities': np.load(example_dir / 'densities.npy'),
+      'num_electrons': np.load(example_dir / 'num_electrons.npy'),
+      'latex_symbols': np.load(example_dir / 'latex_symbols.npy'),
+  }
+  grids = np.load(example_dir / 'grids.npy')
 
-  ksi = ks_inversion.two_iter_KS_inversion(f_grids, f_v_Z_fn, f_tar_density,
-                                           num_electrons)
+  example = tree_util.tree_map(
+      lambda x: x[example['distances'] == separation][0], example)
 
-  f_v_XC = ksi.f_v_XC
-  step_list = ksi.step_list
-  cost_list = ksi.cost_list
-  truncation = ksi.truncation
+  lda_xc = functionals.ExponentialLDAFunctional(grids)
+  lda_v_xc = lda_xc.get_xc_potential(example['densities'])
 
-  # Example plot
-  v_s = ksi._get_v_eff()
+  ksi = ks_inversion.two_iter_KS_inversion(
+      grids,
+      lambda _: example['external_potentials'],
+      example['densities'],
+      example['num_electrons'],
+      init_v_xc=lda_v_xc,
+      mixing_param=0.4,
+      max_iters=200,
+  )
+
+  v_xc = ksi.f_v_XC
   n = ksi.f_density
-  plt.plot(f_grids, f_tar_density, label='exact $n$')
-  plt.plot(f_grids, v_s, label='inversion $v_s$')
-  plt.plot(f_grids, n, '--', label='inversion $n$')
+
+  fig, ax = plt.subplots()
+  ax.plot(grids, example['densities'], label='exact $n$')
+  ax.plot(grids, v_xc, label=r'inversion $v_{XC}$')
+  ax.plot(grids, n, '--', label='inversion $n$')
+  ax.plot(grids, lda_v_xc, label=r'$v^{LDA}_{XC}$')
+
+  ax.set_xlabel(r'$x$')
+  ax.set_xlim(-10, 10)
   plt.legend()
   plt.show()

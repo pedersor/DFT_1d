eaf74b2640e685f79448078c8828b51b469972ca
pedersor <pedersor@uci.edu>
2022-12-05 15:26:33 -0800

    jax-free ofdft

--- DFT_1d/of_dft.py
+++ DFT_1d/of_dft.py
@@ -8,29 +8,29 @@ Note(pedersor): Experimental module!
 from functools import partial
 
 import numpy as np
-import jax
-import jax.numpy as jnp
-import scipy
-from jax.config import config
+from scipy import optimize
 
-import functionals
+from DFT_1d import constants
+from DFT_1d import utils
 
-config.update("jax_enable_x64", True)
+
+def v_ee(
+    grids,
+    A=constants.EXPONENTIAL_COULOMB_AMPLITUDE,
+    k=constants.EXPONENTIAL_COULOMB_KAPPA,
+):
+  vp = A * np.exp(-k * np.abs(grids))
+  return vp
 
 
 def get_dx(grids):
   return grids[1] - grids[0]
 
 
-@partial(jax.jit)
-def _get_hartree_energy(grids, density, v_ee_mat):
-  n1 = jnp.expand_dims(density, axis=0)
-  n2 = jnp.expand_dims(density, axis=1)
-  return 0.5 * jnp.sum(n1 * n2 * v_ee_mat) * get_dx(grids)**2
-
-
-def get_hartree_energy(density, grids, v_ee_mat):
-  return _get_hartree_energy(density, grids, v_ee_mat)
+def get_hartree_energy(grids, density, v_ee_mat):
+  n1 = np.expand_dims(density, axis=0)
+  n2 = np.expand_dims(density, axis=1)
+  return 0.5 * np.sum(n1 * n2 * v_ee_mat) * get_dx(grids)**2
 
 
 class OFDFT(object):
@@ -41,28 +41,32 @@ class OFDFT(object):
       v_ext,
       num_electrons,
       kinetic_en_fn,
+      deriv_kinetic_en_fn,
       xc_energy_density_fn=None,
   ):
     self.grids = grids
     self.v_ext = v_ext
     self.num_electrons = num_electrons
     self.kinetic_en_fn = kinetic_en_fn
+    self.deriv_kinetic_en_fn = deriv_kinetic_en_fn
 
     if xc_energy_density_fn is not None:
 
       self.xc = xc_energy_density_fn
 
       # faster to cache the Hartree matrix
-      r1 = jnp.expand_dims(grids, axis=0)
-      r2 = jnp.expand_dims(grids, axis=1)
-      self.v_ee_mat = functionals.v_ee(r1 - r2)
+      r1 = np.expand_dims(grids, axis=0)
+      r2 = np.expand_dims(grids, axis=1)
+      self.v_ee_mat = v_ee(r1 - r2)
 
       self.energy_fn = self._energy_fn
+      raise NotImplementedError('Hartree and XC energies not implemented yet.')
     else:
       self.energy_fn = self._energy_fn_wo_hxc
+      self.grad_energy_fn = self._grad_energy_fn_wo_hxc
 
     self.optimizer = partial(
-        scipy.optimize.minimize,
+        optimize.minimize,
         method='BFGS',
         options={
             'gtol': 1e-4,
@@ -70,7 +74,6 @@ class OFDFT(object):
         },
     )
 
-  @partial(jax.jit, static_argnums=(0,))
   def _energy_fn(self, phi):
     """ Energy with Hartree and XC energies, i.e. 
     
@@ -80,53 +83,63 @@ class OFDFT(object):
 
     # get density from phi
     density = phi**2
-    density *= self.num_electrons / jnp.trapz(density, self.grids)
+    density *= self.num_electrons / np.trapz(density, self.grids)
 
     ke = self.kinetic_en_fn(density=density, grids=self.grids)
     hartree_en = get_hartree_energy(self.grids, density, self.v_ee_mat)
-    xc_en = jnp.trapz(self.xc(density) * density, self.grids)
-    ext_en = jnp.trapz(self.v_ext * density, self.grids)
+    xc_en = np.trapz(self.xc(density) * density, self.grids)
+    ext_en = np.trapz(self.v_ext * density, self.grids)
     tot_en = ke + hartree_en + xc_en + ext_en
 
     return tot_en
 
-  @partial(jax.jit, static_argnums=(0,))
   def _energy_fn_wo_hxc(self, phi):
     """ Energy without Hartree and XC energies, i.e. E = T_s + V_ext """
 
     # get density from phi
     density = phi**2
-    density *= self.num_electrons / jnp.trapz(density, self.grids)
+    density *= self.num_electrons / np.trapz(density, self.grids)
 
     ke = self.kinetic_en_fn(density, self.grids)
-    ext_en = jnp.trapz(self.v_ext * density, self.grids)
+    ext_en = np.trapz(self.v_ext * density, self.grids)
     tot_en = ke + ext_en
 
     return tot_en
 
-  @partial(jax.jit, static_argnums=(0,))
-  def grad_energy_fn(self, phi):
-    return jax.grad(self.energy_fn)(phi)
+  def _grad_energy_fn_wo_hxc(self, density):
 
-  def np_energy_fn(self, phi):
-    return np.array(self.energy_fn(phi))
+    d_ts = self.deriv_kinetic_en_fn(density, self.grids)
+    d_ext = self.v_ext
 
-  def np_grad_energy_fn(self, phi):
-    return np.array(self.grad_energy_fn(phi))
+    return d_ts + d_ext
+
+  def residual(self, phi):
+
+    density = phi**2
+    density *= self.num_electrons / np.trapz(density, self.grids)
+
+    return (self.grad_energy_fn(phi) - self.get_mu(density)) * phi
+
+  def get_mu(self, density):
+
+    return np.trapz(
+        self.grad_energy_fn(density) * density,
+        self.grids,
+    ) / self.num_electrons
 
   def run(self, guess_density=None):
 
     if guess_density is None:
 
       def gaussian_density(grids):
-        return np.exp(-0.5 * grids**2)
+        return np.exp(-1 * grids**2)
 
       guess_density = gaussian_density(self.grids)
       guess_density = np.where(guess_density < 1e-15, 0, guess_density)
 
     res = self.optimizer(
-        fun=self.np_energy_fn,
-        jac=self.np_grad_energy_fn,
+        fun=self.energy_fn,
+        jac=self.residual,
         x0=guess_density**0.5,
     )
 
@@ -143,26 +156,55 @@ class OFDFT(object):
 
 
 if __name__ == '__main__':
+  # Example using Poschl-Teller potential and Von-Weizsacker kinetic energy
+
   import matplotlib.pyplot as plt
+
+  def poschl_teller_potential(grids, lam):
+    return -(lam * (lam + 1) / 2) / (np.cosh(grids))**2
 
   h = 0.04
   grids = np.arange(-256, 257) * h
-  v_ext = 0.5 * grids**2
+  v_ext = poschl_teller_potential(grids, lam=2)
 
-  def get_tau_vw(density, grids):
+  # defione kinetic energy functionals to use.
+  # note you need to also supply the functional derivative.
+
+
+  def get_tau_vw(density, grids, num_offset):
     """ von Weizsacker (vW) kinetic energy density."""
 
     dx = grids[1] - grids[0]
-    grad_density = jnp.gradient(density, dx)
-    res = (1 / 8) * (grad_density**2) / (density + 1e-10)
-    return jnp.nan_to_num(res)
+    grad_density = np.gradient(density, dx)
+    res = (1 / 8) * (grad_density**2) / (density + num_offset)
+    return np.nan_to_num(res)
 
-  def get_t_vw(density, grids):
-    tau = get_tau_vw(density, grids)
-    t_vw = jnp.trapz(tau, grids)
+  def get_t_vw(density, grids, num_offset=1e-10):
+    tau = get_tau_vw(density, grids, num_offset)
+    t_vw = np.trapz(tau, grids)
     return t_vw
 
-  ofdft_solver = OFDFT(grids, v_ext, num_electrons=2, kinetic_en_fn=get_t_vw)
+  # second derivative matrix (finite differences)
+  d2_mat = utils.DerivativeTool(grids).d2_mat
+
+  def get_deriv_t_vw(density, grids, num_offset=1e-10):
+    dx = grids[1] - grids[0]
+    grad_density = np.gradient(density, dx)
+    grad2_density = d2_mat.dot(density)
+
+    term_1 = (1 / 8) * (grad_density**2 / (density + num_offset)**2)
+    term_2 = -(1 / 4) * (grad2_density / (density + num_offset))
+
+    return term_1 + term_2
+
+  ofdft_solver = OFDFT(
+      grids,
+      v_ext,
+      num_electrons=2,
+      kinetic_en_fn=get_t_vw,
+      deriv_kinetic_en_fn=get_deriv_t_vw,
+  )
+  # can supply a guess density (default is a gaussian)
   energy, density = ofdft_solver.run(guess_density=None)
 
   print('energy = ', energy)

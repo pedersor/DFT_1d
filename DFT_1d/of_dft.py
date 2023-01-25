8be3c5fd5920e6036d65c4005284a281b1a13a9a
pedersor <pedersor@uci.edu>
2023-01-25 11:43:36 -0800

    more stable optimization

--- DFT_1d/of_dft.py
+++ DFT_1d/of_dft.py
@@ -67,7 +67,7 @@ class OFDFT(object):
 
     self.optimizer = partial(
         optimize.minimize,
-        method='BFGS',
+        method='L-BFGS-B',
         options={
             'gtol': 1e-4,
             'maxiter': 500,
@@ -115,10 +115,11 @@ class OFDFT(object):
 
   def residual(self, phi):
 
+    scale = self.num_electrons / np.trapz(phi**2, self.grids)
     density = phi**2
-    density *= self.num_electrons / np.trapz(density, self.grids)
+    density *= scale
 
-    return (self.grad_energy_fn(phi) - self.get_mu(density)) * phi
+    return 2* (phi*scale) * (self.grad_energy_fn(density) - self.get_mu(density))   
 
   def get_mu(self, density):
 
@@ -134,8 +135,9 @@ class OFDFT(object):
       def gaussian_density(grids):
         return np.exp(-1 * grids**2)
 
-      guess_density = gaussian_density(self.grids)
-      guess_density = np.where(guess_density < 1e-15, 0, guess_density)
+      guess_density = gaussian_density(self.grids) + 1e-3
+      #guess_density = np.where(guess_density < 1e-15, 0, guess_density)
+      guess_density *= self.num_electrons / np.trapz(guess_density, self.grids)
 
     res = self.optimizer(
         fun=self.energy_fn,
@@ -144,7 +146,6 @@ class OFDFT(object):
     )
 
     conv_density = res.x**2
-    # normalize
     conv_density *= self.num_electrons / np.trapz(conv_density, self.grids)
     en = res.fun
 
@@ -154,6 +155,26 @@ class OFDFT(object):
 
     return en, conv_density
 
+  def simple_run(self, eta=1e-4, guess_density=None):
+      
+      if guess_density is None:
+  
+        def gaussian_density(grids):
+          return np.exp(-0.5 * grids**2)
+  
+        guess_density = gaussian_density(self.grids)
+        guess_density *= self.num_electrons / np.trapz(guess_density, self.grids)
+  
+      phi = guess_density**0.5
+      for i in range(4000):
+        phi -= eta * self.residual(phi)
+
+        print(self.energy_fn(phi**2))
+
+      density = phi**2
+      en = self.energy_fn(density)
+      
+      return en, density
 
 if __name__ == '__main__':
   # Example using Poschl-Teller potential and Von-Weizsacker kinetic energy
@@ -163,9 +184,10 @@ if __name__ == '__main__':
   def poschl_teller_potential(grids, lam):
     return -(lam * (lam + 1) / 2) / (np.cosh(grids))**2
 
-  h = 0.04
+  h = 0.02
   grids = np.arange(-256, 257) * h
   v_ext = poschl_teller_potential(grids, lam=2)
+  #v_ext = 0.5* grids **2 
 
   # defione kinetic energy functionals to use.
   # note you need to also supply the functional derivative.
@@ -179,7 +201,7 @@ if __name__ == '__main__':
     res = (1 / 8) * (grad_density**2) / (density + num_offset)
     return np.nan_to_num(res)
 
-  def get_t_vw(density, grids, num_offset=1e-10):
+  def get_t_vw(density, grids, num_offset=1e-3):
     tau = get_tau_vw(density, grids, num_offset)
     t_vw = np.trapz(tau, grids)
     return t_vw
@@ -187,7 +209,7 @@ if __name__ == '__main__':
   # second derivative matrix (finite differences)
   d2_mat = utils.DerivativeTool(grids).d2_mat
 
-  def get_deriv_t_vw(density, grids, num_offset=1e-10):
+  def get_deriv_t_vw(density, grids, num_offset=1e-3):
     dx = grids[1] - grids[0]
     grad_density = np.gradient(density, dx)
     grad2_density = d2_mat.dot(density)
